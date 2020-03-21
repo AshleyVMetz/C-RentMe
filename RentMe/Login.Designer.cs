a2a68f05d7eac03b3082fa5f2f1298331cd43168
Ashley Metz <ametz1@my.westga.edu>
2020-03-21 15:13:50 -0600

    Added a login form for admin

---
+++ RentMe/Login.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace RentMe
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.loginAdminButton = new System.Windows.Forms.Button();
+            this.adminLogin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(186, 84);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(77, 17);
+            this.usernameLabel.TabIndex = 0;
+            this.usernameLabel.Text = "Username:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(190, 143);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(73, 17);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(286, 81);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(242, 22);
+            this.usernameTextBox.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(286, 143);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(242, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // loginAdminButton
+            // 
+            this.loginAdminButton.Location = new System.Drawing.Point(341, 195);
+            this.loginAdminButton.Name = "loginAdminButton";
+            this.loginAdminButton.Size = new System.Drawing.Size(96, 42);
+            this.loginAdminButton.TabIndex = 4;
+            this.loginAdminButton.Text = "Login";
+            this.loginAdminButton.UseVisualStyleBackColor = true;
+            this.loginAdminButton.Click += new System.EventHandler(this.loginAdminButton_Click);
+            // 
+            // adminLogin
+            // 
+            this.adminLogin.AutoSize = true;
+            this.adminLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adminLogin.Location = new System.Drawing.Point(237, 9);
+            this.adminLogin.Name = "adminLogin";
+            this.adminLogin.Size = new System.Drawing.Size(291, 55);
+            this.adminLogin.TabIndex = 5;
+            this.adminLogin.Text = "Admin Login";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 282);
+            this.Controls.Add(this.adminLogin);
+            this.Controls.Add(this.loginAdminButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button loginAdminButton;
+        private System.Windows.Forms.Label adminLogin;
+    }
+}
