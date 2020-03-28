58afedd70d9ad04abdbd8bf527c75d1a91ab2662
Tim Bauman <tbauman2@my.westga.edu>
2020-03-28 10:16:57 -0500

    Logout button functionality on AdminDashboard

--- RentMe/View/AdminDashboard.Designer.cs
+++ RentMe/View/AdminDashboard.Designer.cs
@@ -44,21 +44,19 @@
             // 
             this.adminTabControl.Controls.Add(this.addEmployeeTabPage);
             this.adminTabControl.Controls.Add(this.updateEmployeeTabPage);
-            this.adminTabControl.Location = new System.Drawing.Point(16, 49);
-            this.adminTabControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.adminTabControl.Location = new System.Drawing.Point(12, 40);
             this.adminTabControl.Name = "adminTabControl";
             this.adminTabControl.SelectedIndex = 0;
-            this.adminTabControl.Size = new System.Drawing.Size(601, 812);
+            this.adminTabControl.Size = new System.Drawing.Size(451, 660);
             this.adminTabControl.TabIndex = 1;
             // 
             // addEmployeeTabPage
             // 
             this.addEmployeeTabPage.Controls.Add(this.addEmployeeUserControl);
-            this.addEmployeeTabPage.Location = new System.Drawing.Point(4, 25);
-            this.addEmployeeTabPage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.addEmployeeTabPage.Location = new System.Drawing.Point(4, 22);
             this.addEmployeeTabPage.Name = "addEmployeeTabPage";
-            this.addEmployeeTabPage.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.addEmployeeTabPage.Size = new System.Drawing.Size(593, 783);
+            this.addEmployeeTabPage.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.addEmployeeTabPage.Size = new System.Drawing.Size(443, 634);
             this.addEmployeeTabPage.TabIndex = 0;
             this.addEmployeeTabPage.Text = "Add Employee";
             this.addEmployeeTabPage.UseVisualStyleBackColor = true;
@@ -66,19 +64,18 @@
             // addEmployeeUserControl
             // 
             this.addEmployeeUserControl.Location = new System.Drawing.Point(0, 0);
-            this.addEmployeeUserControl.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.addEmployeeUserControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.addEmployeeUserControl.Name = "addEmployeeUserControl";
-            this.addEmployeeUserControl.Size = new System.Drawing.Size(591, 780);
+            this.addEmployeeUserControl.Size = new System.Drawing.Size(443, 634);
             this.addEmployeeUserControl.TabIndex = 2;
             // 
             // updateEmployeeTabPage
             // 
             this.updateEmployeeTabPage.Controls.Add(this.updateEmployeeUserControl);
-            this.updateEmployeeTabPage.Location = new System.Drawing.Point(4, 25);
-            this.updateEmployeeTabPage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.updateEmployeeTabPage.Location = new System.Drawing.Point(4, 22);
             this.updateEmployeeTabPage.Name = "updateEmployeeTabPage";
-            this.updateEmployeeTabPage.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.updateEmployeeTabPage.Size = new System.Drawing.Size(593, 783);
+            this.updateEmployeeTabPage.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.updateEmployeeTabPage.Size = new System.Drawing.Size(443, 634);
             this.updateEmployeeTabPage.TabIndex = 1;
             this.updateEmployeeTabPage.Text = "Update Employee";
             this.updateEmployeeTabPage.UseVisualStyleBackColor = true;
@@ -86,41 +83,39 @@
             // updateEmployeeUserControl
             // 
             this.updateEmployeeUserControl.Location = new System.Drawing.Point(0, 0);
-            this.updateEmployeeUserControl.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.updateEmployeeUserControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.updateEmployeeUserControl.Name = "updateEmployeeUserControl";
-            this.updateEmployeeUserControl.Size = new System.Drawing.Size(591, 780);
+            this.updateEmployeeUserControl.Size = new System.Drawing.Size(443, 634);
             this.updateEmployeeUserControl.TabIndex = 0;
             // 
             // labeluser
             // 
-            this.labeluser.Location = new System.Drawing.Point(16, 14);
-            this.labeluser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labeluser.Location = new System.Drawing.Point(12, 11);
             this.labeluser.Name = "labeluser";
-            this.labeluser.Size = new System.Drawing.Size(493, 28);
+            this.labeluser.Size = new System.Drawing.Size(370, 23);
             this.labeluser.TabIndex = 1;
             this.labeluser.Text = "Logged in user";
             this.labeluser.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // logoutButton
             // 
-            this.logoutButton.Location = new System.Drawing.Point(517, 14);
-            this.logoutButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.logoutButton.Location = new System.Drawing.Point(388, 11);
             this.logoutButton.Name = "logoutButton";
-            this.logoutButton.Size = new System.Drawing.Size(100, 28);
+            this.logoutButton.Size = new System.Drawing.Size(75, 23);
             this.logoutButton.TabIndex = 3;
             this.logoutButton.Text = "Logout";
             this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Click += new System.EventHandler(this.LogoutButton_Click);
             // 
             // AdminDashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(633, 876);
+            this.ClientSize = new System.Drawing.Size(475, 712);
             this.Controls.Add(this.logoutButton);
             this.Controls.Add(this.labeluser);
             this.Controls.Add(this.adminTabControl);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AdminDashboard";
--- RentMe/View/AdminDashboard.cs
+++ RentMe/View/AdminDashboard.cs
@@ -10,10 +10,24 @@ namespace RentMe.View
         /// <summary>
         /// Constructor method.
         /// </summary>
+        /// <param name="username">The username of the logged in user.</param>
         public AdminDashboard(string username)
         {
             InitializeComponent();
             labeluser.Text = username;
+        }
+
+        /// <summary>
+        /// This method logs the user out of the application when the logout button is clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LogoutButton_Click(object sender, System.EventArgs e)
+        {
+            Form loginForm = new EmployeeLogin();
+            this.Hide();
+            loginForm.ShowDialog();
+            this.Close();
         }
     }
 }
